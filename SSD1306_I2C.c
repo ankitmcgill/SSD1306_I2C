2d5a448d57a89152ea0d8527340aef492eb1b5fb
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2017-05-26 16:16:50 +0530

    initial empty source code file commit

---
+++ SSD1306_I2C.c
@@ -0,0 +1,20 @@
+/****************************************************************
+* SSD1306 BASED OLED MODULE (128 x 64)
+* I2C MODE
+*
+* NOTE: THE OLED CONTROLLER SUPPORTS BOTH I2C AND SPI, BUT THE
+* 		MODULES BOUGHT USUALLY HARDWIRE TO SELECT A PARTICULAR
+* 		MODE
+*
+* MAY 26 2017
+*
+* ANKIT BHATNAGAR
+* ANKIT.BHATNAGARINDIA@GMAIL.COM
+*
+* REFERENCES
+* ------------
+*   (1) http://robotcantalk.blogspot.in/2015/03/interfacing-arduino-with-ssd1306-driven.html
+*   (2)	http://robotcantalk.blogspot.in/2015/03/interfacing-arduino-with-ssd1306-driven_9.html
+****************************************************************/
+
+#include "SSD1306_I2C.h"
